c8b7baf85d8dd388e8fe9fab69e3e250018e2a8c
heyMP <mgp140@gmail.com>
2019-11-13 15:03:56 -0500

    fixed null reference id

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
+//@ts-check
 require("dotenv").config();
 const { Photon } = require("@generated/photon");
 const photon = new Photon();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const corsOrigin = process.env.CORS_ORIGIN || '*';
-const apolloServer = require('./apolloServer')
+const corsOrigin = process.env.CORS_ORIGIN || "*";
+const apolloServer = require("./apolloServer");
 
 async function main() {
   const app = express();
-  await photon.connect()
+  await photon.connect();
 
   app.use(cors());
   app.use(bodyParser.text());
@@ -23,40 +24,41 @@ async function main() {
     next();
   });
 
-  app.post('/', async (req, res) => {
+  app.post("/", async (req, res) => {
     try {
-      const { id, values } = req.body
-      const _id = id || ''
-      if (_id) {
+      const { id, values } = req.body;
+      let data = {
+        values: JSON.stringify(values)
+      };
+      if (id) {
         // create the form first reference first
         await photon.forms.upsert({
-          where: { reference: _id },
-          create: { reference: _id },
+          where: { reference: id },
+          create: { reference: id },
           update: {}
-        })
+        });
+        data = {
+          ...data,
+          form: {
+            connect: { reference: id }
+          }
+        };
       }
       const submission = await photon.submissions.create({
-        data: {
-          values: JSON.stringify(values),
-          form: {
-            connect: { reference: _id }
-          },
-        },
+        data,
         include: { form: true }
-      })
-      res.send(submission)
+      });
+      res.send(submission);
     } catch (error) {
-      console.error(error)
-      res.send(error)
+      console.error(error);
+      res.send(error);
     }
-  })
+  });
 
-  apolloServer({ app })
+  apolloServer({ app });
 
   app.listen(4000, () => {
-    console.log(
-      `🚀 Server ready at http://localhost:4000`
-    );
+    console.log(`🚀 Server ready at http://localhost:4000`);
   });
 }
 
@@ -64,4 +66,4 @@ main()
   .catch(e => {
     console.error(e);
   })
-  .finally(async () => { });
+  .finally(async () => {});
