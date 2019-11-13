c75e225e24cf67b5c9b68ad33d5ea76deb85e83d
heyMP <mgp140@gmail.com>
2019-11-13 08:57:24 -0500

    linting 💄

--- components/hax-form/src/HaxForm.js
+++ components/hax-form/src/HaxForm.js
@@ -5,8 +5,8 @@ export class HaxForm extends LitElement {
     return {
       endpoint: { type: String },
       loading: { type: Boolean, reflect: true },
-      formId: { type: String }
-    }
+      formId: { type: String },
+    };
   }
 
   static get styles() {
@@ -18,71 +18,97 @@ export class HaxForm extends LitElement {
   }
 
   constructor() {
-    super()
-    this.endpoint = null
-    this.loading = false
-    this._form = null
-    this.formId = null
+    super();
+    this.endpoint = null;
+    this.loading = false;
+    this._form = null;
+    this.formId = null;
   }
 
+  /**
+   * When the element first sets up add
+   * an event listener to monitor any slot changes.
+   * We do this to gather the children of the component.
+   */
   firstUpdated() {
-    // this.shadowRoot.querySelector('form').addEventListener('submit', this.clickHandler.bind())
-    this.shadowRoot.querySelector('slot').addEventListener('slotchange', this.slotChangedHandler.bind(this))
+    this.shadowRoot
+      .querySelector('slot')
+      .addEventListener('slotchange', this.slotChangedHandler.bind(this));
   }
 
   disconnectedCallback() {
-    this.shadowRoot.querySelector('slot').removeEventListener('slotchange', this.slotChangedHandler.bind(this))
-    super.disconnectedCallback()
+    this.shadowRoot
+      .querySelector('slot')
+      .removeEventListener('slotchange', this.slotChangedHandler.bind(this));
+    super.disconnectedCallback();
   }
 
+  /**
+   * When new children are added look for a form element and
+   * compture all submission events from that form.
+   * @param {event} e 
+   */
   slotChangedHandler(e) {
     // get all children
-    const nodes = e.target.assignedNodes({flatten: true})
-    const form = [...nodes].find(i => i.nodeName === 'FORM')
+    const nodes = e.target.assignedNodes({ flatten: true });
+    const form = [...nodes].find(i => i.nodeName === 'FORM');
     // if there is a form
     if (form) {
-      this._form = form
-      form.addEventListener('submit', e => {
-        e.preventDefault()
-        this.submitForm()
-      })
+      this._form = form;
+      form.addEventListener('submit', _e => {
+        _e.preventDefault();
+        this.submitForm();
+      });
     }
   }
 
+  /**
+   * Collects all of the form values and submits
+   * them to the endpoint
+   */
   submitForm() {
-    const form = this._form
+    const form = this._form;
 
     if (!form) {
-      console.error(`No form found.`)
+      console.error(`No form found.`);
     }
 
     if (this.endpoint) {
-      const values = this.collectFormValues(form)
-      const id = this.formId
+      const values = this.collectFormValues(form);
+      const id = this.formId;
 
       fetch(this.endpoint, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           id,
-          values
-        })
+          values,
+        }),
       })
-        .then(() => this.loading = true)
-        .finally(() => this.loading = false)
-    }
-    else {
-      console.error(`Endpoint not defined`)
+        .then(() => {
+          this.loading = true;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    } else {
+      console.error(`Endpoint not defined`);
     }
   }
 
-  collectFormValues(form) {
-    const formItems = form.querySelectorAll('[name]')
-    const values = [...formItems]
-      .map(i => Object.assign({}, { name: i.name, value: i.value }))
-    return values
+  /**
+   * Collect all values of a form
+   * @param {DOM Node} form 
+   * @return {object}
+   *  - value
+   *  - name
+   */
+  static collectFormValues(form) {
+    const formItems = form.querySelectorAll('[name]');
+    const values = [...formItems].map(i => ({ name: i.name, value: i.value }));
+    return values;
   }
 
   render() {
